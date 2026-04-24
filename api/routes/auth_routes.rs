c1fb25970d314344e919b768d41227c1903a403b
Your Name <macbookpro@192.168.1.6>
2026-04-25 00:53:33 +0500

    added comment

--- api/routes/auth_routes.rs
+++ api/routes/auth_routes.rs
@@ -2,6 +2,7 @@ use axum::{Router, routing::post};
 use mini_webflare::handlers::auth_handler::{signup, login};
 use sqlx::PgPool;
 
+// aunthentication routing
 pub fn auth_route() -> Router<PgPool> {
     Router::new()
         .route("/signup", post(signup))
